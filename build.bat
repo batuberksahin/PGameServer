f70f66280fed8f65ac95588d39bcaacb610897b8
Batu Berk <batuberkshn@gmail.com>
2023-07-20 18:10:24 +0300

    Self contained added

--- build.bat
+++ build.bat
@@ -1,20 +1,26 @@
 @echo off
 
+:: Set the paths and configurations
 set "rootDir=%cd%"
 set "buildsDir=%rootDir%\builds"
 set "gameServerBuildsDir=%buildsDir%\gameServerBuilds"
 set "masterServerBuildsDir=%buildsDir%\masterServerBuilds"
 
+:: Clean previous build directories
 if exist "%gameServerBuildsDir%" rmdir /s /q "%gameServerBuildsDir%"
 if exist "%masterServerBuildsDir%" rmdir /s /q "%masterServerBuildsDir%"
 
-dotnet publish -c Release -r win-x86 --self-contained false -o "%gameServerBuildsDir%\x86" "%rootDir%\GameServer\GameServer.csproj"
+:: Build GameServer for x86 platform
+dotnet publish -c Release -r win-x86 --self-contained true -o "%gameServerBuildsDir%\x86" "%rootDir%\GameServer\GameServer.csproj"
 
-dotnet publish -c Release -r win-x64 --self-contained false -o "%gameServerBuildsDir%\x64" "%rootDir%\GameServer\GameServer.csproj"
+:: Build GameServer for x64 platform
+dotnet publish -c Release -r win-x64 --self-contained true -o "%gameServerBuildsDir%\x64" "%rootDir%\GameServer\GameServer.csproj"
 
-dotnet publish -c Release -r win-x86 --self-contained false -o "%masterServerBuildsDir%\x86" "%rootDir%\MasterServer\MasterServer.csproj"
+:: Build MasterServer for x86 platform
+dotnet publish -c Release -r win-x86 --self-contained true -o "%masterServerBuildsDir%\x86" "%rootDir%\MasterServer\MasterServer.csproj"
 
-dotnet publish -c Release -r win-x64 --self-contained false -o "%masterServerBuildsDir%\x64" "%rootDir%\MasterServer\MasterServer.csproj"
+:: Build MasterServer for x64 platform
+dotnet publish -c Release -r win-x64 --self-contained true -o "%masterServerBuildsDir%\x64" "%rootDir%\MasterServer\MasterServer.csproj"
 
 echo Build completed successfully.
 pause
--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ masterServerBuildsDir="$buildsDir/masterServerBuilds"
 rm -rf "$gameServerBuildsDir"
 rm -rf "$masterServerBuildsDir"
 
-dotnet publish -c Release -r linux-x64 --self-contained false -o "$gameServerBuildsDir/x64" "$rootDir/GameServer/GameServer.csproj"
+dotnet publish -c Release -r linux-x64 --self-contained true -o "$gameServerBuildsDir/x64" "$rootDir/GameServer/GameServer.csproj"
 
-dotnet publish -c Release -r linux-x64 --self-contained false -o "$masterServerBuildsDir/x64" "$rootDir/MasterServer/MasterServer.csproj"
+dotnet publish -c Release -r linux-x64 --self-contained true -o "$masterServerBuildsDir/x64" "$rootDir/MasterServer/MasterServer.csproj"
 
 echo "Build completed successfully."
